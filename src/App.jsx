4b4349a8a6459983fadbdc9d59a5f6bd8445ca04
Admin Portal PSP <admin@kudus.go.id>
2026-06-18 10:31:39 +0700

    fix tahun missing in database payload

--- src/App.jsx
+++ src/App.jsx
@@ -104,6 +104,7 @@ export default function App() {
           kecamatan: itemForm.kecamatan,
           desa: itemForm.lokasi,
           status: itemForm.status,
+          tahun: itemForm.tahun,
           lat: itemForm.lat,
           lng: itemForm.lng,
           foto: itemForm.foto
@@ -126,6 +127,7 @@ export default function App() {
           kecamatan: itemForm.kecamatan,
           desa: itemForm.lokasi,
           status: itemForm.status,
+          tahun: itemForm.tahun,
           lat: itemForm.lat,
           lng: itemForm.lng,
           foto: itemForm.foto
